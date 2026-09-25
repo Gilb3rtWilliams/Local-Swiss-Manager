648a036ee7f137d647d79cffd53d4a288ca37c48
Gilb3rtWilliams <lance6827@gmail.com>
2026-09-25 10:39:51 +0300

    Remove AuthGate component from main rendering, simplifying the application structure.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import AuthGate from "../../desktop/frontend/src/auth/AuthGate.jsx";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthGate>
-        <App />
-      </AuthGate>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
